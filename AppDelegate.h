c000f219e3233a0f3d672a4f30b8a9b251a43e0c
plusluo <182297908@qq.com>
2015-06-11 14:20:53 +0800

    first commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  colorLight
+//
+//  Created by plusluo on 15/6/11.
+//  Copyright (c) 2015年 plusluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  colorLight
+//
+//  Created by plusluo on 15/6/11.
+//  Copyright (c) 2015年 plusluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
